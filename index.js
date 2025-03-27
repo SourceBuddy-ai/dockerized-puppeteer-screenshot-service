fd3fd67ef626364bb3d4a86e86fa71d0af4845fc
Tyler Knell <tyler@Tylers-MacBook-Air.local>
2025-03-27 16:37:34 -0500

    Fix: manually install Chrome and set executablePath

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.post("/screenshot", async (req, res) => {
     console.log("🧠 Launching Puppeteer...");
     const browser = await puppeteer.launch({
       headless: true,
+      executablePath: "/usr/bin/google-chrome-stable",
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
 
